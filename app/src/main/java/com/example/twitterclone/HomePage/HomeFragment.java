82a8d3f1492881d5d9bfbd9af053121a1fdd88e0
XLhunter75 <115193752+XLhunter75@users.noreply.github.com>
2023-01-18 18:02:09 +0100

    Ya se muestra los post que se hace

--- app/src/main/java/com/example/twitterclone/HomePage/HomeFragment.java
+++ app/src/main/java/com/example/twitterclone/HomePage/HomeFragment.java
@@ -1,66 +1,100 @@
 package com.example.twitterclone.HomePage;
 
+import static com.example.twitterclone.MainActivity.MDATABASE;
+
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.twitterclone.AdpTweet.AdpTweet;
+import com.example.twitterclone.ModelUser.TweetModel;
 import com.example.twitterclone.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class HomeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public HomeFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private ArrayList<TweetModel> list_posts;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+
+        list_posts = new ArrayList<>();
+
+        //Recogida de los posts
+        MDATABASE.child("Posts").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot post: snapshot.getChildren()){
+
+                    if(!post.getKey().equals("postCount")){
+
+                        String user_url_profile = "";
+                        String user_poster = "";
+                        String user_uid = "";
+                        String content_post = "";
+                        String image_url = "";
+
+                        for(DataSnapshot data: post.getChildren()){
+
+                            if (data.getKey().equals("User")) {
+                                user_poster = data.getValue().toString();
+                            }
+                            else if (data.getKey().equals("UID")) {
+                                user_uid = data.getValue().toString();
+                            }
+                            else if (data.getKey().equals("textPost")) {
+                                content_post = data.getValue().toString();
+                            }
+                            else if (data.getKey().equals("Image1")) {
+                                image_url = data.getValue().toString();
+                            }
+                            else if (data.getKey().equals("pic")){
+                                user_url_profile = data.getValue().toString();
+                            }
+
+                        }
+
+                        list_posts.add(new TweetModel(user_poster, user_uid, content_post, image_url, user_url_profile));
+
+                    }
+
+                }
+
+                createRecycleProductsA(v, list_posts);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return v;
+    }
+
+    public void createRecycleProductsA(@NonNull View v, ArrayList<TweetModel> list_to_show){
+        AdpTweet adpShop_adaptor_2 = new AdpTweet(v.getContext(), list_to_show);
+        RecyclerView recyclerViewPopular = v.findViewById(R.id.recyclePosts);
+        recyclerViewPopular.setLayoutManager(new LinearLayoutManager(v.getContext(), LinearLayoutManager.VERTICAL, false));
+        recyclerViewPopular.setAdapter(adpShop_adaptor_2);
     }
 }
